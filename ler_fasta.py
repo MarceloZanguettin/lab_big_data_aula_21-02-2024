898c52ed6bc51c893eb26c07c628bbf58009a05a
Marcelo Zanguettin Pereira <100152421+MarceloZanguettin@users.noreply.github.com>
2024-02-22 14:13:24 -0300

    Add files via upload

---
+++ ler_fasta.py
@@ -0,0 +1,19 @@
+# encoding:utf-8
+
+def ler_fasta(arquivo):
+    sequencia = ''
+    lista = []
+    with open(arquivo, 'r') as fasta:
+            sequencia = ''
+            for linha in fasta:
+                if not linha.startswith('>'):
+                    sequencia += linha
+            lista.append(sequencia)
+
+    return lista
+
+# testando...
+lista = ler_fasta("sequence.fasta")
+
+for i in lista:
+    print(i)
